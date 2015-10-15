c2492aa6e05b48b210eaf0e1e4a0bab2fedf85cf
Jonathan Torres <jonathantorres41@gmail.com>
2015-10-14 23:29:44 -0400

    Refactor tests.

--- tests/MediumTest.php
+++ tests/MediumTest.php
@@ -11,6 +11,14 @@ class MediumTest extends PHPUnit
 {
     protected $medium;
     protected $mediumClient;
+    protected $authorizationCode = '1234567890';
+    protected $credentials = [
+        'client-id' => 'CLIENT-ID',
+        'client-secret' => 'CLIENT-SECRET',
+        'redirect-url' => 'http://someurl.com/callback',
+        'state' => 'somesecret',
+        'scopes' => 'scope1,scope2',
+    ];
 
     protected function setUp()
     {
@@ -20,15 +28,7 @@ class MediumTest extends PHPUnit
 
     public function testConnectToApiInConstructor()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1, scope2',
-        ];
-
-        $medium = new Medium($credentials);
+        $medium = new Medium($this->credentials);
 
         $this->assertEquals('CLIENT-ID', $medium->getClientId());
         $this->assertEquals('CLIENT-SECRET', $medium->getClientSecret());
@@ -36,16 +36,8 @@ class MediumTest extends PHPUnit
 
     public function testConnectToApiWithConnectMethod()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1, scope2',
-        ];
-
         $medium = new Medium();
-        $medium->connect($credentials);
+        $medium->connect($this->credentials);
 
         $this->assertEquals('CLIENT-ID', $medium->getClientId());
         $this->assertEquals('CLIENT-SECRET', $medium->getClientSecret());
@@ -69,95 +61,50 @@ class MediumTest extends PHPUnit
 
     public function testGetAuthenticationUrl()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
-
-        $medium = new Medium();
-        $medium->connect($credentials);
+        $this->medium->connect($this->credentials);
 
         $expectedUrl = 'https://medium.com/m/oauth/authorize?client_id=CLIENT-ID&' .
                        'scope=scope1%2Cscope2&state=somesecret&response_type=code&' .
                        'redirect_uri=http%3A%2F%2Fsomeurl.com%2Fcallback';
-        $authenticationUrl = $medium->getAuthenticationUrl();
+        $authenticationUrl = $this->medium->getAuthenticationUrl();
 
         $this->assertEquals($expectedUrl, $authenticationUrl);
     }
 
     public function testAuthentication()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
+        $this->authenticationMocks();
 
-        $this->mediumClient->shouldReceive('requestAccessToken')->once()
-                           ->with('1234567890', 'CLIENT-ID', 'CLIENT-SECRET', 'http://someurl.com/callback')
-                           ->andReturn('ACCESS-TOKEN');
-        $this->mediumClient->shouldReceive('authenticate')->once()
-                           ->with('ACCESS-TOKEN')->andReturnNull();
+        $this->medium->connect($this->credentials);
+        $this->medium->setClient($this->mediumClient);
+        $this->medium->authenticate($this->authorizationCode);
 
-        $authorizationCode = '1234567890';
-        $medium = new Medium();
-        $medium->connect($credentials);
-        $medium->setClient($this->mediumClient);
-        $medium->authenticate($authorizationCode);
-
-        $this->assertEquals('ACCESS-TOKEN', $medium->getAccessToken());
+        $this->assertEquals('ACCESS-TOKEN', $this->medium->getAccessToken());
     }
 
     public function testGetRefreshToken()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
-
         $this->mediumClient->shouldReceive('exchangeRefreshToken')->once()
                            ->with('1234567890', 'CLIENT-ID', 'CLIENT-SECRET')
                            ->andReturn('NEW-ACCESS-TOKEN');
 
         $refreshToken = '1234567890';
-        $medium = new Medium();
-        $medium->connect($credentials);
-        $medium->setClient($this->mediumClient);
-        $accessToken = $medium->exchangeRefreshToken($refreshToken);
+        $this->medium->connect($this->credentials);
+        $this->medium->setClient($this->mediumClient);
+        $accessToken = $this->medium->exchangeRefreshToken($refreshToken);
 
         $this->assertEquals('NEW-ACCESS-TOKEN', $accessToken);
     }
 
     public function testGetAuthenticatedUser()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
-
-        $this->mediumClient->shouldReceive('requestAccessToken')->once()
-                           ->with('1234567890', 'CLIENT-ID', 'CLIENT-SECRET', 'http://someurl.com/callback')
-                           ->andReturn('ACCESS-TOKEN');
-        $this->mediumClient->shouldReceive('authenticate')->once()
-                           ->with('ACCESS-TOKEN')->andReturnNull();
+        $this->authenticationMocks();
         $this->mediumClient->shouldReceive('makeRequest')->once()
                            ->with('GET', 'me')->andReturn(new StdClass);
 
-        $authorizationCode = '1234567890';
-        $this->medium->connect($credentials);
+        $this->medium->connect($this->credentials);
         $this->medium->setClient($this->mediumClient);
-        $this->medium->authenticate($authorizationCode);
+        $this->medium->authenticate($this->authorizationCode);
 
         $user = $this->medium->getAuthenticatedUser();
         $this->assertNotNull($user);
@@ -165,14 +112,6 @@ class MediumTest extends PHPUnit
 
     public function testCreateUserPost()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
-
         $postData = [
             'title' => 'Post title',
             'contentFormat' => 'html',
@@ -182,18 +121,13 @@ class MediumTest extends PHPUnit
 
         $requestData['form_params'] = $postData;
 
-        $this->mediumClient->shouldReceive('requestAccessToken')->once()
-                           ->with('1234567890', 'CLIENT-ID', 'CLIENT-SECRET', 'http://someurl.com/callback')
-                           ->andReturn('ACCESS-TOKEN');
-        $this->mediumClient->shouldReceive('authenticate')->once()
-                           ->with('ACCESS-TOKEN')->andReturnNull();
+        $this->authenticationMocks();
         $this->mediumClient->shouldReceive('makeRequest')->once()
                            ->with('POST', 'users/12345/posts', $requestData)->andReturn(new StdClass);
 
-        $authorizationCode = '1234567890';
-        $this->medium->connect($credentials);
+        $this->medium->connect($this->credentials);
         $this->medium->setClient($this->mediumClient);
-        $this->medium->authenticate($authorizationCode);
+        $this->medium->authenticate($this->authorizationCode);
 
         $post = $this->medium->createPost('12345', $postData);
         $this->assertNotNull($post);
@@ -201,14 +135,6 @@ class MediumTest extends PHPUnit
 
     public function testUploadImage()
     {
-        $credentials = [
-            'client-id' => 'CLIENT-ID',
-            'client-secret' => 'CLIENT-SECRET',
-            'redirect-url' => 'http://someurl.com/callback',
-            'state' => 'somesecret',
-            'scopes' => 'scope1,scope2',
-        ];
-
         $requestData = [
             'multipart' => [
                 [
@@ -219,20 +145,24 @@ class MediumTest extends PHPUnit
             ]
         ];
 
+        $this->authenticationMocks();
+        $this->mediumClient->shouldReceive('makeRequest')->once()
+                           ->with('POST', 'images', $requestData)->andReturn(new StdClass);
+
+        $this->medium->connect($this->credentials);
+        $this->medium->setClient($this->mediumClient);
+        $this->medium->authenticate($this->authorizationCode);
+
+        $image = $this->medium->uploadImage('imagedata', 'myimage.jpg');
+        $this->assertNotNull($image);
+    }
+
+    private function authenticationMocks()
+    {
         $this->mediumClient->shouldReceive('requestAccessToken')->once()
                            ->with('1234567890', 'CLIENT-ID', 'CLIENT-SECRET', 'http://someurl.com/callback')
                            ->andReturn('ACCESS-TOKEN');
         $this->mediumClient->shouldReceive('authenticate')->once()
                            ->with('ACCESS-TOKEN')->andReturnNull();
-        $this->mediumClient->shouldReceive('makeRequest')->once()
-                           ->with('POST', 'images', $requestData)->andReturn(new StdClass);
-
-        $authorizationCode = '1234567890';
-        $this->medium->connect($credentials);
-        $this->medium->setClient($this->mediumClient);
-        $this->medium->authenticate($authorizationCode);
-
-        $image = $this->medium->uploadImage('imagedata', 'myimage.jpg');
-        $this->assertNotNull($image);
     }
 }
